afd0efc155d1eab3e971af749e269b15e37fdcc2
marcmcast <56266594+marcmcast@users.noreply.github.com>
2021-01-10 16:25:10 +0100

    KU2
    The AImove script is attached to the AI block which is to move to the target. The seeker is referenced to generate the path and and an instance of it generated as well for the path and a path is generated using the position of the AI block annd the targets position. Then in the start method the MoveTowardsTarget coroutine is called with the position of this.gameobject as its parameters. Firstly a vector3 list is created with the vector path and using a counter is looped throughout each position. A while loop is looped if the distance between the block and next position is larger or equal to 0.5. The block moves one unit towards the position in the list and then the path is refresed with new coordinates just in case that the target moves in future versions. 0.1 seconds are passed until this is looped again until all positions in the list have been visited.

---
+++ Resources/Scripts/AImove.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class AImove : MonoBehaviour
+{
+    //Generate path
+    Seeker seeker;
+
+    //Stores the path
+    Path path;
+
+    //Referencing the target
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Instance of seeker attached to gameobject
+        seeker = GetComponent<Seeker>();
+
+        //Generating first path
+        path = seeker.StartPath(transform.position, target.position);
+
+        //Start the process of moving the AI block to the target
+        StartCoroutine(MoveTowardsTarget(this.transform));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator MoveTowardsTarget(Transform theAI)
+    {
+
+        while (true)
+        {
+
+            List<Vector3> pos = path.vectorPath;
+            Debug.Log("Positions Count: " + pos.Count);
+
+            for (int counter = 0; counter < pos.Count; counter++)
+            {
+                if (pos[counter] != null)
+                {
+                    while (Vector3.Distance(theAI.position, pos[counter]) >= 0.5f)
+                    {
+                        //The AI block moves 1 unit towards the position in the pos list
+                        theAI.position = Vector3.MoveTowards(theAI.position, pos[counter], 1f);
+
+                        //Path refrshed everytime in case code is updated for target to move
+                        path = seeker.StartPath(theAI.position, target.position);
+
+                        //Wait until new path is generated
+                        yield return seeker.IsDone();
+
+                        //The paths direction is updated
+                        pos = path.vectorPath;
+
+                        yield return new WaitForSeconds(0.1f);
+                    }
+
+                }
+                yield return null;
+            }
+            
+        }
+    }
+
+}
