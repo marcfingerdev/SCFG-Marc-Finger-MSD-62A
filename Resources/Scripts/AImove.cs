5bb7842fe9481f02eebf0b7dadccae255da918cf
marcfingerdev <56266594+marcmcast@users.noreply.github.com>
2021-01-10 17:57:58 +0100

    KU2-b
    Firstly a public gameobject of obstacle is referenced. Then a generateObstacles method is created.  Random X and Y coordinates within the range of the camera view are generated and created into a vector3. This position is then used to instaniate the gameobject in the random position. This is looped 5 times in the start method as required. Then next in the start method the referenced graphParent is used to scan the  grid with the new obstacles. The grid is scanned only once after the obstacles are inistaniated, if needed to update continously, a coroutine is needed.

--- Resources/Scripts/AImove.cs
+++ Resources/Scripts/AImove.cs
@@ -14,9 +14,24 @@ public class AImove : MonoBehaviour
     //Referencing the target
     public Transform target;
 
+    //Referencing the obstacles
+    public GameObject obstacle;
+
+    //Referencing PointGraphObject
+    GameObject graphParent;
+
     // Start is called before the first frame update
     void Start()
     {
+        for (int i = 0; i < 5; i++) { 
+            generateObstacles(obstacle);
+        }
+
+        graphParent = GameObject.Find("AStar");
+
+        //Generating component
+        graphParent.GetComponent<AstarPath>().Scan();
+
         //Instance of seeker attached to gameobject
         seeker = GetComponent<Seeker>();
 
@@ -31,6 +46,17 @@ public class AImove : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void generateObstacles(GameObject obstacle)
+    {
+        float randomX = Mathf.Floor(Random.Range(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).y, Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height)).y));
+
+        float randomY = Mathf.Floor(Random.Range(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).x, Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0)).x));
+
+        Vector3 randomLocation = new Vector3(randomX, randomY);
+
+        Instantiate(obstacle, randomLocation, Quaternion.identity);
     }
 
     IEnumerator MoveTowardsTarget(Transform theAI)
